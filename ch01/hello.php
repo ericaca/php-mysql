d89e1689ba69fd8540c68042f0023a24068c7c58
erica <ericaca43@gmail.com>
2017-08-06 23:29:24 +0800

    update ch01

---
+++ ch01/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE相關資訊 html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>將PHP程式嵌入HTML網頁</title>
+</head>
+<body>
+	<?php
+	echo("Hello World");
+	phpinfo();  //顯示php資訊
+	?>
+</body>
+</html>
---
+++ ch01/hello2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>我的第一個php程式</title>
+</head>
+<body>
+	<?php
+	include_once("demo.inc");
+	?>
+</body>
+</html>
